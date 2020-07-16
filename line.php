7cec6a41acd8166dc4f367e44d1f27a1f17fe99e
Wanchaii <68370028+Wanchaii@users.noreply.github.com>
2020-07-16 14:56:53 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = '0VYPUEjsFudHXz5q+yyxZDktyROMnw3IEWLSE9bA1oiXRAcTnOuV/XZIdr5LzKeM50TX4109q23osw0rtLzSFd8yrJNBEwQjSOnWO9vXhwKBFQQPsZ/DknjyjA0pfcq73OhePXX7kaRVtORgxsCFFQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
